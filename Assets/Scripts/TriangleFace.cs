bc212509eb86680996ef582b686f119f4ff38af3
Andre Ferreira <andre.ferreira.v2@gmail.com>
2019-04-15 18:54:24 +0100

    Early exit vertex search if too late to be true

--- Assets/Scripts/TriangleFace.cs
+++ Assets/Scripts/TriangleFace.cs
@@ -48,12 +48,12 @@ public class TriangleFace {
                 }
             }
             
-            if(similar + targetVertices - 2 < i) {
+            if( similar - targetVertices + 2 < i ) {
                 //Debug.Log("We're in cycle " + i + " with only " + similar + " similar and whishing to reach " + targetVertices);
                 return false;
             }
         }
-        return similar == targetVertices;
+        return true;
     }
     
     public static bool areVerticesEqual(Vector3 v1, Vector3 v2) {
